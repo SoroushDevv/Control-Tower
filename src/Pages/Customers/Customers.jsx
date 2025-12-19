ae5dc969a10f894ac0dc16e985de27641eab4669
soroushmoradi <soroushmoradi13.sm@gmail.com>
2025-12-19 17:34:20 +0000

    add customers

--- src/Pages/Customers/Customers.jsx
+++ src/Pages/Customers/Customers.jsx
@@ -16,10 +16,39 @@ export default function Customers () {
                 <h1 className=""></h1>
             </div>
             <div className="">
-                <div></div>
-                <div></div>
-                <div></div>
+                <div>
+                    <div></div>
+                    <div>
+                        <h2></h2>
+                    </div>
+                    <div>
+                        <span className="numbers"></span>
+                        <span className="percent"></span>
+                    </div>
+                </div>
+                <div>
+                    <div></div>
+                    <div>
+                        <h2></h2>
+                    </div>
+                    <div>
+                        <span className="numbers"></span>
+                        <span className="percent"></span>
+                    </div>
+                </div>
+                <div>
+                    <div></div>
+                    <div>
+                        <h2></h2>
+                    </div>
+                    <div>
+                        <span className="numbers"></span>
+                        <span className="percent"></span>
+                    </div>
+                </div>
             </div>
+            
+
         </div>
     )
 }
