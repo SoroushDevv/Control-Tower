91e77ba911e72849df2ea706fa4f6baad8c4c0d1
soroushmoradi <soroushmoradi13.sm@gmail.com>
2025-12-20 10:10:07 +0000

    Add Users table to overview customers

--- src/Pages/Customers/Customers.jsx
+++ src/Pages/Customers/Customers.jsx
@@ -5,8 +5,9 @@ import { Banknote } from 'lucide-react';
 import { TrendingDown } from 'lucide-react';
 import { TrendingUp } from 'lucide-react';
 import { Plus } from 'lucide-react';
-
-
+import { X } from 'lucide-react';
+import Badge from "./../../Components/UI/Badge/Badge"
+import UsersTable from "../../Components/UI/Table/UsersTable";
 
 
 
@@ -17,9 +18,10 @@ export default function Customers() {
     return (
         <div className="bg-light-bg-surface dark:bg-dark-bg-surface dark:text-dark-text-primary overflow-y-auto h-screen w-full">
             <div className="w-full h-fit">
-                <p className="py-2 px-4 ct-title">اطلاهات کلی مشتری ها</p>
+                <p className="relative py-2 px-4 ct-title">اطلاهات کلی مشتری ها
+                </p>
             </div>
-            <div className="w-full h-fit grid grid-cols-1 lg:grid-cols-3 md:grid-cols-3 items-stretch gap-6 p-2">
+            <div className="w-full h-fit grid grid-cols-1 lg:grid-cols-3 md:grid-cols-3 items-stretch gap-6 p-2 mb-10">
                 <div className="border border-light-border rounded-2xl dark:border-dark-border p-4 shadow-lg">
                     <div className="size-8 rounded-full flex justify-center items-center bg-gray-200 border-gray-400 mb-4">
                         <Users className="stroke-light-icon-accentFill size-4" />
@@ -70,18 +72,20 @@ export default function Customers() {
                     </div>
                 </div>
             </div>
-            <div className="flex flex-col divide-y-2  ">
-                <div className="w-full flex justify-between items-center py-2 px-4">
-                    <span className="ct-title">لیست فعالیت مشتری ها</span>
+            <div className="flex flex-col divide-y-2 border border-dark-border rounded-md ">
+                <div className="w-full flex justify-between items-center py-4 px-6">
+                    <span className="ct-title">لیست مشتری ها
+                        <Badge style="mr-2"> 547 تا</Badge>
+                    </span>
                     <div className="flex gap-2">
                         <select className="py-2 px-4 border border-dark-border dark:border-light-border rounded-md cursor-pointer dark:text-dark-text-primary dark:bg-dark-bg-surface">
 
-                            <option className="rounded-md dark:text-dark-text-primary dark:bg-dark-bg-surface cursor-pointer" value="فیلتر" defaultValue={"انتخاب فیلتر..."}>انتخاب فیلتر...</option>
+                            <option className="rounded-md dark:text-dark-text-primary dark:bg-dark-bg-surface cursor-pointer" value="فیلتر">انتخاب فیلتر...</option>
                             <option className="rounded-md dark:text-dark-text-primary dark:bg-dark-bg-surface cursor-pointer" value="جدیدترین">جدیدترین</option>
                             <option className="rounded-md dark:text-dark-text-primary dark:bg-dark-bg-surface cursor-pointer" value="قدیمی ترین">قدیمی ترین</option>
                             <option className="rounded-md dark:text-dark-text-primary dark:bg-dark-bg-surface cursor-pointer" value="بیشترین خرید">بیشترین خرید</option>
                         </select>
-                        <div className="group flex items-center gap-2 overflow-hidden transition-all duration-300 ease-in-out p-3 pr-5 border border-blue-950 bg-blue-600 text-white rounded-md cursor-pointer max-w-fit">
+                        <div className="group flex items-center gap-2 overflow-hidden transition-all duration-300 ease-in-out p-2 pr-4 border border-blue-950 bg-blue-600 text-white rounded-md cursor-pointer max-w-fit">
                             <Plus />
                             <button className="max-w-0 opacity-0 whitespace-nowrap transition-all duration-300 ease-in-out group-hover:max-w-xs group-hover:opacity-100">
                                 اضافه کردن یوزر جدید
@@ -91,16 +95,29 @@ export default function Customers() {
 
 
                 </div>
-                <div className="">
-                    <div className="search user">
-                        <input type="search" />
+                <div className="flex justify-between items-center py-4 px-6">
+                    <div className="py-2 px-4 border border-dark-border-light flex justify-center gap-1 rounded-full">
+                        <button className="-translate-y-1">
+                            پیش فرض
+                        </button>
+                        <X />
                     </div>
-                    <select className="filter user loyalty">
+                    <div className="flex gap-2">
+                        <div className="border border-dark-border dark:border-light-border rounded-full">
+                            <input type="text" className="w-full h-full py-2 px-4 rounded-full" placeholder="دنبال چه میگردی ..." />
+                        </div>
+                        <select className="py-2 px-4 border border-dark-border dark:border-light-border rounded-md cursor-pointer dark:text-dark-text-primary dark:bg-dark-bg-surface">
+                            <option className={"rounded-md dark:text-dark-text-primary dark:bg-dark-bg-surface cursor-pointer"}>همه کاربرها...</option>
+                            <option className={"rounded-md dark:text-dark-text-primary dark:bg-dark-bg-surface cursor-pointer"}>کاربران فعال</option>
+                            <option className={"rounded-md dark:text-dark-text-primary dark:bg-dark-bg-surface cursor-pointer"}>ادمین ها</option>
+                            <option className={"rounded-md dark:text-dark-text-primary dark:bg-dark-bg-surface cursor-pointer"}>vip ها</option>
+                        </select>
+                    </div>
 
-                    </select>
+
                 </div>
-                <div className="users table">
-                    {/* table */}
+                <div className="w-full py-4 px-6">
+                    <UsersTable />
                 </div>
             </div>
         </div>
