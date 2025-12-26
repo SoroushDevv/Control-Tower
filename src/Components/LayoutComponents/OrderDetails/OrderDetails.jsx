a10d8686db1a74f6cab0d1b6f33b869420ba01d2
soroushmoradi <soroushmoradi13.sm@gmail.com>
2025-12-26 15:07:59 +0000

    Add Order status

--- src/Components/LayoutComponents/OrderDetails/OrderDetails.jsx
+++ src/Components/LayoutComponents/OrderDetails/OrderDetails.jsx
@@ -3,7 +3,9 @@ import ProductGallery from "../ProductDetails/Product/ProductGallery";
 import { ArrowDownToLine } from 'lucide-react';
 import OrderProductsTable from "@/Components/UI/Table/OrderProductsTable";
 import OrderPriceSummary from "./OrderPriceSummary";
-
+import { FileX } from 'lucide-react';
+import { MapPin } from 'lucide-react';
+import OrderProgress from "./OrderProgress";
 
 const OrderDetails = () => {
 
@@ -55,11 +57,11 @@ const OrderDetails = () => {
                 </div>
             </div>
             <div className="col-span-1 lg:col-span-3">
-                <div className="grid grid-cols-1 grid-rows-2">
+                <div className="grid grid-cols-1 grid-rows-2 gap-3">
                     <div className="col-span-1 flex flex-col gap-3 border border-light-border rounded-md">
-                        <div className="w-full flex justify-between items-center py-3 px-4 border border-light-border-light">
-                            <span className="text-lg font-bold">سفارش #ORD-7241</span>
-                            <button className="flex justify-center items-center gap-2 py-1 px-3 bg-light-primary hover:bg-dark-primary-subtle  rounded-sm text-light-bg-base transition-all duration-200 ease-in-out">
+                        <div className="w-full flex justify-between items-center py-4 px-6 border border-light-border-light">
+                            <span className="text-lg font-bold">سفارش <span className="text-light-text-secondary">#ORD-7241</span> </span>
+                            <button className="flex justify-center items-center gap-2 py-2 px-4 bg-blue-500 hover:bg-blue-700  rounded-sm text-light-bg-base transition-all duration-200 ease-in-out">
                                 <span className="">فاکتور</span>
                                 <ArrowDownToLine />
                             </button>
@@ -67,7 +69,7 @@ const OrderDetails = () => {
                         <div className="py-3 px-4">
                             <OrderProductsTable />
                         </div>
-                        <div className="w-fit py-3 px-4 border border-t-2 border-dotted">
+                        <div className="w-fit py-3 px-4 border-t-2 border-dotted">
                             <OrderPriceSummary
                                 items={orders}
                                 discountPercent={15}
@@ -77,7 +79,27 @@ const OrderDetails = () => {
                             />
                         </div>
                     </div>
-                    <div className="col-span-1"></div>
+                    <div className="col-span-1 h-fit border border-light-border rounded-md divide-y-2">
+                        <div className="flex justify-between items-center py-4 px-6">
+                            <span className="text-lg font-bold">وضعیت سفارش</span>
+
+                            <div className="flex justify-center items-center gap-2 text-dark-text-primary">
+                                <div className="flex justify-center items-center gap-3 py-2 px-4 bg-red-500 hover:bg-red-700 rounded-md transition-all ease-in-out duration-200 cursor-pointer">
+                                    <button className="">لغو سفارش</button>
+                                    <FileX />
+                                </div>
+                                <div className="flex justify-center items-center gap-3 py-2 px-4 bg-blue-500 hover:bg-blue-700 rounded-md transition-all ease-in-out duration-200 cursor-pointer">
+                                    <button >تغییر ادرس</button>
+                                    <MapPin />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="w-full h-full flex justify-center items-center gap-3 py-2 px-4">
+                            
+                            <OrderProgress />
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
